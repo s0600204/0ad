8490e6ce25493e68e9e666312a49a54a7a92a207
s0600204 <dev@s0600204>
2017-07-31 14:12:31 +0100

    Test set of gui elements.
    Adds one of every gui element with a scrollbar attribute to the mainmenu, to be used for testing.
    
    NOT TO BE COMMITTED INTO SVN

--- binaries/data/mods/public/gui/pregame/mainmenu.js
+++ binaries/data/mods/public/gui/pregame/mainmenu.js
@@ -18,6 +18,33 @@ function init(data, hotloadData)
 			g_BackgroundLayerData,
 			g_ProjectInformation,
 			g_CommunityButtons);
+
+
+	let tstCt = [ "blue", "red", "purple", "cyan", "rainbow", "tartan", "plaid", "brown", "black", "grey", "gray", "pink", "green", "magenta", "taupe", "mauve", "pecan", "orange" ];
+	let tstDd = Engine.GetGUIObjectByName("testDrop");
+	tstDd.list = tstCt;
+	tstDd.selected = 2;
+
+	let tstLt = Engine.GetGUIObjectByName("testList");
+	tstLt.list = tstCt;
+	tstLt.selected = 4;
+
+	let tstOLt = Engine.GetGUIObjectByName("testOList");
+	tstOLt.list_name = tstCt;
+	tstOLt.list_a = tstCt;
+	tstOLt.list_b = tstCt.reverse();
+	tstOLt.list = tstCt;
+	tstOLt.selected = 1;
+
+	let tstTxt = Engine.GetGUIObjectByName("testTextbox").caption;
+	let tstTB = Engine.GetGUIObjectByName("testTextboxScrollable");
+	for (let i=0; i<3; ++i)
+		tstTB.caption += '[font="sans-bold-12"]Scrollable [/font]' + tstTxt + "\n\n";
+
+	let tstML = Engine.GetGUIObjectByName("testInputMultiline");
+	for (let i=0; i<24; ++i)
+		tstML.caption += "\n.";
+	tstML.caption += "\nGroovy, baby!";
 }
 
 function getHotloadData()
